54b44fdd712762f814a33abf5ca2ea493ed64a6b
skyblueyoshi <32297476+skyblueyoshi@users.noreply.github.com>
2022-11-23 17:58:14 +0800

    Update BlockData.lua

--- apis/BlockData.lua
+++ apis/BlockData.lua
@@ -1,10 +1,10 @@
----@class BlockData
----@field textureLocation TextureLocation
----@field group int
----@field subGroup int
----@field stepSoundId int
----@field stepSoundGroupId int
----@field functionSoundId int
+---@class BlockData 描述一个方块数据。
+---@field textureLocation TextureLocation 方块渲染在地图中的纹理。
+---@field group int 方块组，决定渲染在地图中纹理衔接方式。
+---@field subGroup int 方块子组。
+---@field stepSoundId int 踩在方块上发出的音效ID。
+---@field stepSoundGroupId int 踩在方块上发出的音效组ID。
+---@field functionSoundId int 
 ---@field functionSoundGroupId int
 ---@field functionSoundId2 int
 ---@field functionSoundGroupId2 int
@@ -12,18 +12,19 @@
 ---@field functionSoundGroupId3 int
 ---@field functionSoundId4 int
 ---@field functionSoundGroupId4 int
----@field isRipen boolean
----@field isDoorOpened boolean
----@field isDoorClosed boolean
+---@field isRipen boolean 是否允许催熟。
+---@field isDoorOpened boolean 是否为开启了的门。
+---@field isDoorClosed boolean 是否为关闭了的门。
 local BlockData = {}
 
----CanSeed
----@param itemID int
----@return boolean
+---判断指定物品，能否种植在当前方块上。
+---@param itemID int 物品ID。
+---@return boolean 物品能否种植在方块上。
 function BlockData:CanSeed(itemID)
 end
 
----@return boolean
+---当前方块是否拥有动画效果。
+---@return boolean 是否拥有动画效果。
 function BlockData:HasAnimation()
 end
 
