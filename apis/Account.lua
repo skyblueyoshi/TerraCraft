c2dd5d060f034095bf6919362458d9b4a548b867
skyblueyoshi <32297476+skyblueyoshi@users.noreply.github.com>
2022-11-23 17:33:32 +0800

    Update Account.lua

--- apis/Account.lua
+++ apis/Account.lua
@@ -1,9 +1,10 @@
----@class Account
----@field name string
----@field skinID int
+---@class Account 描述一个账号信息。
+---@field name string 当前账号的名称。
+---@field skinID int 当前账号的皮肤ID。
 local Account = {}
 
----@return Account
+---创建一个账号对象。
+---@return Account 新的账号对象。
 function Account.new()
 end
 
